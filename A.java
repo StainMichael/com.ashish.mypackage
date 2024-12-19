df2a8ff3fc9c3bad041f3dca2c3684ab17a03daa
Ashish Kumar Mohanta <ashishmohanta52@gmail.com>
2024-12-19 21:59:22 +0530

    main

---
+++ A.java
@@ -0,0 +1,11 @@
+public class A {
+    A(int x, String y) {
+        System.out.println(x);
+        System.out.println(y);
+    }
+
+    public static void main(String[] args) {
+        A a1 = new A(10, "Hello");
+
+    }
+}
